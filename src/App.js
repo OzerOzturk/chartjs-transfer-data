6d99c49fcccd6c13cacd2cd672a1faf9d041c7cd
OzerOzturk <ozerozturk1636@gmail.com>
2022-06-12 23:34:43 +0300

    add barChart and lineChart for the data.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import BarChart from "./components/BarChart";
+import LineChart from "./components/LineChart";
+import { TeamData } from "./Data";
 
 function App() {
+  const [teamData, setTeamData] = useState({
+    labels: TeamData.map((data) => data.year),
+    datasets: [
+      {
+        label: "Team Income",
+        data: TeamData.map((data) => data.teamIncome),
+        backgroundColor: ["rgb(119, 231, 73)"],
+        borderColor: "gray",
+        borderWidth: 1,
+      },
+      {
+        label: "Team Expenditure",
+        data: TeamData.map((data) => data.teamExpenditure),
+        backgroundColor: ["rgb(212, 46, 46)"],
+        borderColor: "gray",
+        borderWidth: 1,
+      },
+    ],
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <div>
+        <a href="https://www.fcporto.pt/en">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/tr/5/52/FC.Porto.png"
+            alt=""
+            width="50"
+            style={{ marginRight: 20 }}
+          />
         </a>
-      </header>
+
+        <h1 style={{ fontFamily: "monospace" }}>FC Porto Transfer Data</h1>
+      </div>
+
+      <div style={{ width: 800, margin: "auto", marginBottom: 40 }}>
+        <BarChart chartData={teamData} />
+      </div>
+      <div style={{ width: 800, margin: "auto", marginBottom: 20 }}>
+        <LineChart chartData={teamData} />
+      </div>
+      <p>
+        <span style={{ fontStyle: "italic", fontWeight: "bold" }}>
+          Information
+        </span>
+        : Figures in the charts are expressed in million euros. Data were
+        fetched from
+        <a
+          style={{ "text-decoration": "none" }}
+          href="https://www.transfermarkt.com/"
+        >
+          transfermarket.com
+        </a>
+      </p>
     </div>
   );
 }
---
+++ src/Data.js
@@ -0,0 +1,46 @@
+//FC Porto Transfer Datas (Arrivals / Departures) based on related years. Costs are in millions of euros. Data fetched from transfermarket.com
+
+export const TeamData = [
+  {
+    id: 1,
+    year: "2015 / 2016",
+    teamIncome: 133.15,
+    teamExpenditure: 44.5,
+  },
+  {
+    id: 2,
+    year: "2016 / 2017",
+    teamIncome: 15.06,
+    teamExpenditure: 40.65,
+  },
+  {
+    id: 3,
+    year: "2017 / 2018",
+    teamIncome: 70.2,
+    teamExpenditure: 24.89,
+  },
+  {
+    id: 4,
+    year: "2018 / 2019",
+    teamIncome: 72.55,
+    teamExpenditure: 40.06,
+  },
+  {
+    id: 5,
+    year: "2019 / 2020",
+    teamIncome: 88.0,
+    teamExpenditure: 63.25,
+  },
+  {
+    id: 6,
+    year: "2020 / 2021",
+    teamIncome: 76.15,
+    teamExpenditure: 22.73,
+  },
+  {
+    id: 7,
+    year: "2021 / 2022",
+    teamIncome: 72.25,
+    teamExpenditure: 34.85,
+  },
+];
